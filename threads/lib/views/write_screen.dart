6cfbe425f1f98354595f305eeb2f32b9687b4cc0
devgony <devgony@gmail.com>
2023-08-31 15:02:27 +0900

    Day25-Write Screen

--- threads/lib/views/write_screen.dart
+++ threads/lib/views/write_screen.dart
@@ -1,15 +1,159 @@
-import 'package:flutter/widgets.dart';
+import 'package:faker/faker.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class PostScreen extends StatelessWidget {
-  const PostScreen({Key? key}) : super(key: key);
+import '../constants/sizes.dart';
+import '../utils.dart';
 
-  static const String routeURL = '/post';
-  static const String routeName = 'post';
+class WriteScreen extends StatefulWidget {
+  const WriteScreen({Key? key}) : super(key: key);
 
   @override
+  State<WriteScreen> createState() => _WriteScreenState();
+}
+
+class _WriteScreenState extends State<WriteScreen> {
+  final _textController = TextEditingController();
+  final faker = Faker();
+  final profile = getImage();
+  late String username;
+
+  @override
+  void initState() {
+    username = faker.person.name();
+    super.initState();
+    _textController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  // static const String routeURL = '/write';
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('post'),
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(28),
+        // color: Colors.grey.shade300,
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Center(
+              child: Text(
+                "Cancel",
+                style: TextStyle(fontSize: Sizes.size16),
+              ),
+            ),
+          ),
+          leadingWidth: 80,
+          backgroundColor: Colors.transparent,
+          title: const Text('New thread'),
+          bottom: const PreferredSize(
+            preferredSize: Size.fromHeight(1),
+            child: Divider(
+              height: 24,
+              thickness: 1,
+            ),
+          ),
+        ),
+        body: IntrinsicHeight(
+          child: Row(
+            // mainAxisSize: MainAxisSize.min,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      foregroundImage: NetworkImage(
+                        profile,
+                      ),
+                      radius: 24,
+                    ),
+                    Expanded(
+                      child: VerticalDivider(
+                        width: Sizes.size32,
+                        thickness: 0.5,
+                        color: Colors.grey.shade600,
+                        indent: 5,
+                        endIndent: 5,
+                      ),
+                    ),
+                    CircleAvatar(
+                      foregroundImage: NetworkImage(
+                        profile,
+                      ),
+                      radius: 12,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 5,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      username,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextField(
+                      controller: _textController,
+                      decoration: const InputDecoration(
+                        hintText: "Start a thread...",
+                        border: InputBorder.none,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Icon(
+                        FontAwesomeIcons.paperclip,
+                        color: Colors.grey.shade600,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        bottomSheet: BottomAppBar(
+          // height: 64,
+          elevation: 0,
+          // color: Colors.transparent,
+          // margin: const EdgeInsets.only(bottom: 24),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12,
+            // vertical: 24,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Anyone can reply",
+                style: TextStyle(fontSize: 16, color: Colors.grey.shade600),
+              ),
+              Text(
+                "Post",
+                style: TextStyle(
+                  color: Colors.blue
+                      .withOpacity(_textController.text.isEmpty ? 0.3 : 1),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
