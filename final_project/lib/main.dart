a891de4836e64197e701cd3ab048a5fd1cbf50cc
devgony <devgony@gmail.com>
2023-09-24 22:53:25 +0900

    Day48-Final Project Setup

---
+++ final_project/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:final_project/views/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+void main() {
+  GoRouter.optionURLReflectsImperativeAPIs = true;
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
