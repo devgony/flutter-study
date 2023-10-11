8656e47dd49d0ac01126793ac6c9b6f2f5cb8c66
devgony <devgony@gmail.com>
2023-10-11 14:19:02 +0900

    build: create final_animation project

---
+++ final_animation/lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+      ),
+      body: const Text("yay"),
+    );
+  }
+}
